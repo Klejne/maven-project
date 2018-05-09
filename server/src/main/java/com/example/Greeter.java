9229298662a97e46cab04faa59e7290249174f3d
Klejne <klejne@users.noreply.github.com>
2018-05-09 11:48:27 +0200

    ret ting
    ret de ting som jenkins siger

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,4 +1,4 @@
-package-info.java
+package com.example;
 
 /**
  * This is a class.
@@ -12,7 +12,7 @@ public class Greeter {
 
   }
 
-  //TODO: her skal være noget?
+  //her skal være noget?
   private String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
